1e47d17268721e9eb9d3da946e2819a0731053ec
Donkings382 <donk4692@gmail.com>
2026-09-12 14:16:11 +0100

    updated the runtime-config.js

--- spaceX project/runtime-config.js
+++ spaceX project/runtime-config.js
@@ -1,8 +1,14 @@
 (function () {
   var current = window.SPACEX_CONFIG || {};
+  var isLocalHost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+  var defaultApiBase = isLocalHost
+    ? "http://localhost:4000"
+    : "https://spacex-backend-yxlo.onrender.com";
 
   window.SPACEX_CONFIG = {
-    apiBase: current.apiBase || "http://localhost:4000",
+    apiBase: current.apiBase || defaultApiBase,
     smartsuppKey: current.smartsuppKey || "REPLACE_WITH_SMARTSUPP_SITE_KEY",
   };
 
